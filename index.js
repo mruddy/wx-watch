cecb1975eed66dd91996646d001345a49c9eb39b
mruddy <mruddy@users.noreply.github.com>
2021-08-13 11:28:27 -0400

    update code to use socket.io 4.1.3

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ const forkStationProcess = function() {
       wx = msg;
       console.log(new Date().toISOString() + ', update received, instant=' + new Date(wx.instant).toISOString());
       // tell all of the connected clients about the update
-      Object.keys(io.sockets.sockets).forEach(function(socketIndex, arrIndex, arr) {
-        io.sockets.connected[socketIndex].volatile.emit('wx', wx);
+      io.sockets.sockets.forEach((value) => {
+        value.volatile.emit('wx', wx);
       });
     }
   });
